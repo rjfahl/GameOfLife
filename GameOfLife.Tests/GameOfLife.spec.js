8df408e47fc51a38097165e29a52762ac05b3cea
Rob Fahl <rfahl@apterainc.com>
2017-01-16 14:40:06 -0500

    Initial GameOfLife commit.

---
+++ GameOfLife.Tests/GameOfLife.spec.js
@@ -0,0 +1,72 @@
+﻿/*
+ * Rules of the game;
+ *   o the Game of Life is an infinete two-dimensional grid of square cells
+ *   o each cell can be alive or dead
+ *   o every cell interacts with its eight neighbors either horizontally, vertically, or diagonally
+ *   o Generation Transition Rules:
+ *     1. Any live cell with < 2 live neighbors dies, due to underpopulation
+ *     2. Any live cell with 2 or 3 live neighbors lives to the next generation
+ *     3. Any live cell with > 3 live neighbors dies, due to overpopulation
+ *     4. Any dead cell with exactly three live neighbors becomes a live cell, due to reproduction
+ *   o give an initial pattern or seed for the system to run on
+ *   o the first generation is created by applying the above rules simultaneously to every cell in the seed
+ *     - births and deaths occur simultaneously
+ */
+
+describe("Game Of Life", function () {
+    var gameOfLife;
+
+    beforeEach(function () {
+        gameOfLife = new GameOfLife();
+    });
+
+    describe("given the seed, then first generation is created accurated", function () {
+        generateGenerations(1, 'still life',
+            [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+            [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]);
+
+        generateGenerations(1, 'Oscillator Blinker',
+            [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]);
+
+        generateGenerations(1, 'Oscillator Toad',
+            [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 1, 0, 0, 1, 0], [0, 1, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]]);
+
+        generateGenerations(1, 'Oscillator Beacon',
+            [[0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0]]);
+
+        generateGenerations(1, 'Spaceships Glider',
+            [[0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0], [0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]);
+    });
+
+    describe("given the seed and number of generations, then given number of generations are created", function () {
+        generateGenerations(2, 'still life',
+            [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+            [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]);
+
+        generateGenerations(2, 'Oscillator Blinker',
+            [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]);
+
+        generateGenerations(2, 'Oscillator Toad',
+            [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]);
+
+        generateGenerations(2, 'Oscillator Beacon',
+            [[0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0]],
+            [[0, 0, 0, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 1, 1, 0, 0, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0]]);
+
+        //generateGenerations(2, 'Spaceships Glider',
+        //    [[0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0], [0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]],
+        //    [[0, 0, 0, 0, 0, 0], [0, 1, 0, 1, 0, 0], [0, 0, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]);
+    });
+
+    function generateGenerations(genCount, message, seed, generation) {
+        it("can create the {genCount} generation {message}".replace('{genCount}', genCount).replace('{message}', message), function () {
+            expect(gameOfLife.Play(seed, genCount)).toEqual(generation);
+        });
+    }
+});
---
+++ GameOfLife/GameOfLife.js
@@ -0,0 +1,121 @@
+﻿function GameOfLife() {
+    var seed;
+
+    function Play(initSeed, genCount) {
+        seed = initSeed;
+        return CreateGenerations(genCount);
+    }
+
+    function CreateGenerations(genCount) {
+        for (var i = 0; i < genCount; i++) {
+            SanitizeSeed();
+            var generation = CreateGeneration(initializeGeneration());
+            seed = generation;
+        }
+        return generation;
+    }
+
+    function CreateGeneration(newGeneration) {
+        for (var y = 0; y < seed.length; y++)
+            for (var x = 0; x < seed[y].length; x++)
+                newGeneration[y][x] = determineLifeInNextGeneration(y, x);
+        return newGeneration;
+    }
+
+    function SanitizeSeed() {
+        cleanTop();
+        cleanBottom();
+        cleanLeft();
+        cleanRight();
+    }
+
+    function cleanTop() {
+        for (var i = 0; i < seed[0].length; i++)
+            if (seed[0][i] == 1) {
+                seed.unshift(new Array(seed[0].length));
+                return;
+            }
+    }
+
+    function cleanBottom() {
+        for (var i = 0; i < seed[seed.length - 1].length; i++)
+            if (seed[seed.length - 1][i] == 1) {
+                seed.push(new Array(seed[seed.length - 1].length));
+                return;
+            }
+    }
+
+    function cleanLeft() {
+        for (var y = 0; y < seed.length; y++)
+            if (seed[y][0] == 1) {
+                for (var l = 0; l < seed.length; l++)
+                    seed[l].unshift(0);
+                return;
+            }
+    }
+
+    function cleanRight() {
+        for (var y = 0; y < seed.length; y++)
+            if (seed[y][seed[y].length - 1] == 1) {
+                for (var l = 0; l < seed.length; l++) 
+                    seed[l].push(0);
+                return;
+            }
+    }
+
+    function initializeGeneration() {
+        var generation = new Array(seed.length)
+        for (var i = 0; i < seed.length; i++) 
+            generation[i] = new Array(seed[0].length);        
+        return generation;
+    }
+
+    function determineLifeInNextGeneration(y, x) {
+        var total = findLivingHorizontalNeighbors(y, x) + findLivingVerticalNeighbors(y, x) + findLivingDiagonalNeighbors(y, x);
+        if (total == 3)
+            return 1;
+        else if (seed[y][x] == 1)
+            return total < 2 || total > 3 ? 0 : 1;
+        else
+            return 0;
+    }
+
+    function findLivingHorizontalNeighbors(y, x) {
+        var left, right = 0;
+        try {
+            left = seed[y][x - 1] == undefined ? 0 : seed[y][x - 1];
+            right = seed[y][x + 1] == undefined ? 0 : seed[y][x + 1];
+        } catch (ex) { }
+        return left + right;
+    }
+
+    function findLivingVerticalNeighbors(y, x) {
+        var top, bottom = 0;
+        try {
+            top = seed[y - 1][x] == undefined ? 0 : seed[y - 1][x];
+            bottom = seed[y + 1][x] == undefined ? 0 : seed[y + 1][x];
+        } catch (ex) { }
+        return top + bottom;
+    }
+
+    function findLivingDiagonalNeighbors(y, x) {
+        var topLeft, topRight, bottomLeft, bottomRight = 0;
+        try {
+            topLeft = seed[y - 1][x - 1] == undefined ? 0 : seed[y - 1][x - 1];
+            topRight = seed[y - 1][x + 1] == undefined ? 0 : seed[y - 1][x + 1];
+            bottomLeft = seed[y + 1][x - 1] == undefined ? 0 : seed[y + 1][x - 1];
+            bottomRight = seed[y + 1][x + 1] == undefined ? 0 : seed[y + 1][x + 1];
+        } catch (ex) { }
+        return topLeft + topRight + bottomLeft + bottomRight;
+    }
+
+    function displayGeneration(generation) {
+        for (var y = 0; y < generation.length; y++)
+            console.log(generation[y].toString());
+        console.log("\n");
+    }
+
+    return {
+        Play: Play
+    }
+}
