bda5ee4e05518da55df63aff5d46584d50b6bec5
Rob Fahl <rfahl@apterainc.com>
2017-01-17 17:36:03 -0500

    Created an interactive front end for the GameOfLife

---
+++ application.js
@@ -0,0 +1,178 @@
+angular.module("GameOfLife", [])
+    .controller('AngularController', ['$interval', function ($interval) {
+        var AngularVC = this;
+        AngularVC.Vertical = 40;
+        AngularVC.Horizontal = 40;
+        AngularVC.RefreshRate = 200;
+        AngularVC.IsPaused = true;
+        AngularVC.Grid = new Grid(AngularVC.Vertical, AngularVC.Horizontal);
+        console.log(AngularVC.Grid);
+
+        AngularVC.CustomGrids = [
+            {
+                "Name": "Gosper Glider Gun",
+                "Grid": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+                        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+            },
+
+        ];
+
+        AngularVC.SelectedCustomGridName = "Template";
+        AngularVC.SelectedCustomGrid = function (index) {
+            AngularVC.Vertical = AngularVC.CustomGrids[index].Grid.length;
+            AngularVC.Horizontal = AngularVC.CustomGrids[index].Grid[0].length;
+            AngularVC.SelectedCustomGridName = AngularVC.CustomGrids[index].Name;
+            AngularVC.Grid = AngularVC.CustomGrids[index].Grid;
+        }
+
+        AngularVC.ClearGrid = function () {
+            AngularVC.SelectedCustomGridName = "Template";
+            AngularVC.Grid = new Grid(AngularVC.Vertical, AngularVC.Horizontal);
+        };
+        AngularVC.PlayGame = function () {
+            AngularVC.IsPaused = false;
+            stop = $interval(function () {
+                if (!AngularVC.IsPaused) {
+                    console.log("Playing");
+                    Play();
+                } else {
+                    AngularVC.StopPlaying();
+                }
+            }, AngularVC.RefreshRate);
+        };
+        AngularVC.StopPlaying = function () {
+            if (angular.isDefined(stop)) {
+                console.log("Paused");
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        }
+        AngularVC.ToggleCell = function (ver, hor) {
+            console.log("Toggle", ver, hor);
+            if (AngularVC.Grid[ver][hor] == 0)
+                AngularVC.Grid[ver][hor] = 1;
+            else
+                AngularVC.Grid[ver][hor] = 0;
+        }
+        AngularVC.LogGrid = function () {
+            console.log("Grid:", AngularVC.Grid.toString());
+        }
+
+        return AngularVC;
+
+        function Grid(vertical, horizontal) {
+            var Grid;
+            Grid = GenerateGrid(vertical, horizontal);
+            return Grid;
+
+            function GenerateGrid(vertical, horizontal) {
+                var generation = new Array(vertical)
+                for (var i = 0; i < vertical; i++)
+                    generation[i] = Array.apply(null, Array(horizontal)).map(Number.prototype.valueOf, 0);
+                return generation;
+            }
+        }
+
+        function Play() {
+            CreateGeneration();
+
+            function CreateGeneration() {
+                var newGrid = new Grid(AngularVC.Grid.length, AngularVC.Grid[0].length);
+                for (var y = 0; y < AngularVC.Grid.length; y++)
+                    for (var x = 0; x < AngularVC.Grid[y].length; x++)
+                        newGrid[y][x] = determineLifeInNextGeneration(y, x);
+                AngularVC.Grid = newGrid;
+            }
+
+            function determineLifeInNextGeneration(y, x) {
+                var total = findLivingHorizontalNeighbors(y, x) + findLivingVerticalNeighbors(y, x) + findLivingDiagonalNeighbors(y, x);
+                if (total == 3)
+                    return 1;
+                else if (AngularVC.Grid[y][x] == 1)
+                    return total < 2 || total > 3 ? 0 : 1;
+                else
+                    return 0;
+            }
+
+            function findLivingHorizontalNeighbors(y, x) {
+                var left, right = 0;
+                try {
+                    left = AngularVC.Grid[y][x - 1] == undefined ? 0 : AngularVC.Grid[y][x - 1];
+                    right = AngularVC.Grid[y][x + 1] == undefined ? 0 : AngularVC.Grid[y][x + 1];
+                } catch (ex) { }
+                return left + right;
+            }
+
+            function findLivingVerticalNeighbors(y, x) {
+                var top, bottom = 0;
+                try {
+                    top = AngularVC.Grid[y - 1][x] == undefined ? 0 : AngularVC.Grid[y - 1][x];
+                    bottom = AngularVC.Grid[y + 1][x] == undefined ? 0 : AngularVC.Grid[y + 1][x];
+                } catch (ex) { }
+                return top + bottom;
+            }
+
+            function findLivingDiagonalNeighbors(y, x) {
+                var topLeft, topRight, bottomLeft, bottomRight = 0;
+                try {
+                    topLeft = AngularVC.Grid[y - 1][x - 1] == undefined ? 0 : AngularVC.Grid[y - 1][x - 1];
+                    topRight = AngularVC.Grid[y - 1][x + 1] == undefined ? 0 : AngularVC.Grid[y - 1][x + 1];
+                    bottomLeft = AngularVC.Grid[y + 1][x - 1] == undefined ? 0 : AngularVC.Grid[y + 1][x - 1];
+                    bottomRight = AngularVC.Grid[y + 1][x + 1] == undefined ? 0 : AngularVC.Grid[y + 1][x + 1];
+                } catch (ex) { }
+                return topLeft + topRight + bottomLeft + bottomRight;
+            }
+
+            function Grid(vertical, horizontal) {
+                var Grid;
+                Grid = GenerateGrid(vertical, horizontal);
+                return Grid;
+
+                function GenerateGrid(vertical, horizontal) {
+                    var generation = new Array(vertical)
+                    for (var i = 0; i < vertical; i++)
+                        generation[i] = Array.apply(null, Array(5)).map(Number.prototype.valueOf, 0);
+                    return generation;
+                }
+            }
+        }
+    }]);
+
